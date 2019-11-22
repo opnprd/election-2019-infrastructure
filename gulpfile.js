25754739bfe13a1c2770209dc2712f0c5d5637d1
Giles Dring <giles@dringtech.com>
2019-11-22 21:35:50 +0000

    Adding typescript build system

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
 const { createReadStream } = require('fs');
 const { src, dest, series } = require('gulp');
+const ts = require('gulp-typescript');
 const zip = require('gulp-zip');
 
 const { getFileHash, getS3Hash } = require('./util/hash');
 const { writeFile } = require('./util/async-fs');
 const { s3 } = require('./util/s3');
 
+const tsProject = ts.createProject('tsconfig.json');
+
+function compile () {
+  return tsProject.src()
+    .pipe(tsProject());
+}
+
 function package () {
   return src([
-    'src/**/*.js',
+    'build/**/*.js',
   ])
     .pipe(zip('lambda.zip', { modifiedTime: new Date('2019/12/12') }))
     .pipe(dest('build'));
@@ -16,13 +24,14 @@ function package () {
 
 async function publish () {
   const filePath = './build/lambda.zip';
+  const versionPath = './build/version';
 
   const latestS3Version = await getS3Hash({ Bucket: 'odileeds-code-staging', Prefix: 'election-2019/lambda.zip' });
   const localHash = await getFileHash(filePath);
 
   if (latestS3Version.hash === localHash) {
     console.log('Package file has not changed');
-    await writeFile('./build/.version', latestS3Version.version);
+    await writeFile(versionPath, latestS3Version.version);
     return;    
   }
 
@@ -34,11 +43,12 @@ async function publish () {
     Body: fileStream,
   }).promise();
 
-  await writeFile('./build/.version', uploadResult.VersionId);
+  await writeFile(versionPath, uploadResult.VersionId);
 }
 
 module.exports = {
-  default: series(package, publish),
+  default: series(compile, package, publish),
+  compile,
   package,
   publish,
 };
