03548b3d7ee41a7946a86e5321dad81137320ea3
Giles Dring <giles@dringtech.com>
2019-11-22 14:35:56 +0000

    Attempting to read from S3

--- src/lambda/index.js
+++ src/lambda/index.js
@@ -1,6 +1,17 @@
+const { S3 } = require('AWS-SDK');
+
+const s3 = new S3();
+
+const bucketName = 'odileeds-uk-election-2019';
+const prefix = 'results/';
+
 async function enrich(event, context) {
-  console.log(event);
-  console.log(context);
+  // TODO: deal with continuations if > max number.
+  const results = await s3.listObjectsV2({
+    Bucket: bucketName,
+    Prefix: prefix,
+  }).promise();
+  console.dir(results);
 }
 
 module.exports = {
