f322417b3c024d651028b59281c54185129091ef
Giles Dring <giles@dringtech.com>
2019-11-22 14:57:52 +0000

    Mapping lambda results

--- src/lambda/index.js
+++ src/lambda/index.js
@@ -7,11 +7,20 @@ const prefix = 'results/';
 
 async function enrich(event, context) {
   // TODO: deal with continuations if > max number.
-  const results = await s3.listObjectsV2({
-    Bucket: bucketName,
-    Prefix: prefix,
-  }).promise();
-  console.dir(results);
+  const resultFiles = [];
+  const pageSize = 10;
+  // let truncated;
+  // do {
+    const results = await s3.listObjectsV2({
+      Bucket: bucketName,
+      Prefix: prefix,
+      MaxKeys: pageSize,
+    }).promise();
+    resultFiles.push(results.Contents.map(x => x.Key));
+    // ({ IsTruncated: truncated }) = results;
+  // } while(truncated);
+
+  console.dir(resultFiles);
 }
 
 module.exports = {
