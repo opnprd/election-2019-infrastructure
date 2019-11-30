2a5c8d48e7f487805838a0d91e329dc433de002e
Giles Dring <giles@dringtech.com>
2019-11-30 23:29:39 +0000

    Adding batch operation

--- src/lambda/index.ts
+++ src/lambda/index.ts
@@ -1,17 +1,33 @@
 import * as s3 from './lib/s3';
+import { promises } from 'dns';
 
 const bucketName = 'odileeds-uk-election-2019';
 const bucketPath = 'public/results/';
+
+function buildProcessor(key) {
+  return async () => {
+    const content = await s3.getObjectContents({ bucket: bucketName, path: key });
+    // Reshape and add reference
+    await s3.putObjectContents({ bucket: bucketName, path: key }, content);
+  }
+}
+
+function batcher(acc, current, index) {
+  const batch = Math.floor(index/10);
+  acc.length = batch + 1;
+  if (!Array.isArray(acc[batch])) acc[batch] = [];
+  acc[batch] = [ ...acc[batch], current ];
+  return acc;
+}
 
 export async function enrich(event, context) {
   const resultFiles = await s3.getObjectList({
     bucket: bucketName,
     path: bucketPath,
   })
-  // TODO Get this to return a function, which is executed in batches
-  const processFile = resultFiles.map(async (key) => {
-    const content = await s3.getObjectContents({ bucket: bucketName, path: key });
-    console.log(content);
-  })
-  await Promise.all(processFile);
+  const processors = resultFiles.map(buildProcessor).reduce(batcher, []);
+  while (processors.length) {
+    const batch = processors.shift();
+    await Promise.all(batch.map((x: () => void) => x()))
+  }
 }
