65607d4a569a5069940fe167eda412dc271951f5
Giles Dring <giles@dringtech.com>
2019-12-01 00:14:14 +0000

    Fix type issue

--- src/lambda/index.ts
+++ src/lambda/index.ts
@@ -7,7 +7,7 @@ const outputBucketPath = 'processed/';
 const summaryFile = outputBucketPath + '2019-results.csv';
 
 function buildProcessor(key: string) {
-  return async () => {
+  return async () : Promise<[string, string]> => {
     const filename = basename(key);
     const resultSet = await s3.getObjectContents({ bucket: bucketName, path: key }).then(JSON.parse);
     // Reshape and add reference
@@ -18,7 +18,7 @@ function buildProcessor(key: string) {
   }
 }
 
-function batcher(acc, current: any, index: number) {
+function batcher(acc, current: any, index: number) : any[] {
   const batch = Math.floor(index/10);
   acc.length = batch + 1;
   if (!Array.isArray(acc[batch])) acc[batch] = [];
@@ -35,7 +35,7 @@ export async function enrich(event, context) {
   const results = [[ 'ccode', 'first19' ]];
   while (processors.length) {
     const batch = processors.shift();
-    const result = await Promise.all(batch.map(x => x()))
+    const result : string[][] = await Promise.all(batch.map(x => x()))
     results.push(...result);
   }
   const summaryCsv = results.map(x => x.join(',')).join('\n');
