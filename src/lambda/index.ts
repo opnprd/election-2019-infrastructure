1e7f16233a96c58eadb1af0d11f9888854061080
Giles Dring <giles@dringtech.com>
2019-12-02 23:43:08 +0000

    Fix location of candidate data

--- src/lambda/index.ts
+++ src/lambda/index.ts
@@ -11,7 +11,7 @@ function buildProcessor(key: string) {
     const filename = basename(key);
     const resultSet = await s3.getObjectContents({ bucket: bucketName, path: key }).then(JSON.parse);
     // Reshape and add reference
-    const { id, candidates } = resultSet;
+    const { id, elections: { 2019: { candidates } } = resultSet;
     const winner = candidates.sort((a,b) => b.votes - a.votes)[0].party.code;
     await s3.putObjectContents({ bucket: bucketName, path: outputBucketPath + filename }, JSON.stringify(resultSet), 'public-read');
     return [ id, winner ];
