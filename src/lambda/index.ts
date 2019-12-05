2c564951e2cabdc491037fa2c93bb6ec25aafe0e
Giles Dring <giles@dringtech.com>
2019-12-05 18:40:44 +0000

    Add 5 minute trigger on summarise lambda function

--- src/lambda/index.ts
+++ src/lambda/index.ts
@@ -89,3 +89,34 @@ export async function enrich(event, context) {
     s3.putObjectContents({ bucket: bucketName, path: atomFeed}, feed.atom1(), { acl: 'public-read', contentType: 'application/atom+xml' }),
   ]);
 }
+
+function resultReader(key: string) {
+  return async () : Promise<[string, string]> => {
+    const resultSet = await s3.getObjectContents({ bucket: bucketName, path: key }).then(JSON.parse);
+    const { id, winner: { party = {}} = {}} = resultSet;
+    // const feedItem = { date: new Date(summary.date), link, title: summary.message };
+    // feed.addItem(feedItem);
+    // const winner = candidates.sort((a,b) => b.votes - a.votes)[0].party.code;
+    return [ id, party.code ];
+  }
+}
+
+export async function summarise(event, context) {
+  const resultFiles = await s3.getObjectList({
+    bucket: bucketName,
+    path: bucketPath,
+  });
+  const processors = resultFiles.map(resultReader).reduce(batcher, []);
+  const results = [[ 'ccode', 'first19' ]];
+  while (processors.length) {
+    const batch = processors.shift();
+    const result : string[][] = await Promise.all(batch.map(x => x()))
+    results.push(...result);
+  }
+  const summaryCsv = results.map(x => x.join(',')).join('\n');
+  await Promise.all([
+    s3.putObjectContents({ bucket: bucketName, path: summaryFile }, summaryCsv, { acl: 'public-read', contentType: 'text/csv' }),
+    s3.putObjectContents({ bucket: bucketName, path: rssFeed}, feed.rss2(), { acl: 'public-read', contentType: 'application/rss+xml' }),
+    s3.putObjectContents({ bucket: bucketName, path: atomFeed}, feed.atom1(), { acl: 'public-read', contentType: 'application/atom+xml' }),
+  ]);
+}
