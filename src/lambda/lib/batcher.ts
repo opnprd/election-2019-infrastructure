65a491ddd8f2f11af030332504ec9d5f252d20ee
Giles Dring <giles@dringtech.com>
2019-12-06 01:54:27 +0000

    Set batch size to 50

--- src/lambda/lib/batcher.ts
+++ src/lambda/lib/batcher.ts
@@ -1,5 +1,6 @@
+const batchSize = 50;
 export default function batcher(acc, current: any, index: number) : any[] {
-  const batch = Math.floor(index/10);
+  const batch = Math.floor(index/batchSize);
   acc.length = batch + 1;
   if (!Array.isArray(acc[batch])) acc[batch] = [];
   acc[batch] = [ ...acc[batch], current ];
