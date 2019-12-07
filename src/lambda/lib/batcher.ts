414b73647cdf5b037a8940c858f987eb42cf588f
Giles Dring <giles@dringtech.com>
2019-12-07 21:03:23 +0000

    Switching batch size to env variable

--- src/lambda/lib/batcher.ts
+++ src/lambda/lib/batcher.ts
@@ -1,4 +1,4 @@
-const batchSize = 1;
+const batchSize = parseInt(process.env.BATCH_SIZE);
 export default function batcher(acc, current: any, index: number) : any[] {
   const batch = Math.floor(index/batchSize);
   acc.length = batch + 1;
